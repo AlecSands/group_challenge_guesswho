c512717b1de91ec82676867371eabf8696e6a6a6
Cat Yanish <catyanish@gmail.com>
2017-06-26 16:34:28 -0500

    appended div with images of group onto DOM

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # group_challenge_guesswho
+
+--[X]Get everyone's picture on the Dom by appending a div
+--[] create varaibles equal to each div
+--[X] Assign data id to each div
+--[] Create prompt
+--[] Create random function
+--[] Click listener for picture
+--[] IF/Else statement OR switch statement -- this compares id of pic to id of random name
+--[]  
---
+++ client.js
@@ -0,0 +1,14 @@
+console.log('Javascript');
+
+$('document').ready(function() {
+  console.log('document ready');
+  $('.container').append('<div data-name="emily"><img src="https://github.com/emilykuplic.png"></div>');
+  $('.container').append('<div data-name="james"><img src="https://github.com/jamesleary.png"></div>');
+  $('.container').append('<div data-name="alec"><img src="https://github.com/AlecSands.png"></div>');
+  $('.container').append('<div data-name="chase"><img src="https://github.com/chasewhitney.png"></div>');
+  $('.container').append('<div data-name="catherine"><img src="https://github.com/CatYanish.png"></div>');
+
+
+
+
+});
